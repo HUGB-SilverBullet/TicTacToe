84d41feccf7f7537fefa941f7d5f085559f94d20
Thordur Bjorn Agustsson <doddi@i-a4933858.greenqloud.internal>
2014-10-29 14:12:18 +0000

    Updated TicTacToeTest.java file.

---
+++ src/test/java/TicTacToeTest.java
@@ -0,0 +1,11 @@
+package TicTacToe;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TicTacToeTest{
+
+	public static void main(String args[]){
+		org.junit.runner.JUnitCore.main("TicTacToe.TicTacToeTest");
+	}
+}
