266b41d2aaf2fb17f4a7f220cab8dc8cbdf0b698
Hjalti Leifsson <hjalti@i-a4933858.greenqloud.internal>
2014-10-30 16:11:54 +0000

    Added a start to spark functionality and test

---
+++ src/main/java/is/ru/tictactoe/TicTacToeWeb.java
@@ -0,0 +1,27 @@
+package is.ru.tictactoe;
+
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication:
+
+public class TicTacToeWeb implements SparkApplication {
+
+	public static void main(String[] args){
+		staticFileLocation("/public");
+		SparkApplication TicTacToeWeb = new TicTacToeWeb();
+		String port = System.getenv("PORT");
+		if(port != null){
+			setPort(Integer.valueOf(port));
+		} 
+		TicTacToeWeb.init();
+	}
+	
+	public void init(){
+	
+		final TicTacToe TTT = new TicTacToe();
+
+		post(new Route("/play"){
+			
+		}
+	}
+}
