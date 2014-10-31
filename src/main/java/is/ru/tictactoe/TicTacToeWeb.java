62bc8cf99405db0c70ace2b7be8353ee40f328f4
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2014-10-31 01:42:11 +0000

    Another error fix

--- src/main/java/is/ru/tictactoe/TicTacToeWeb.java
+++ src/main/java/is/ru/tictactoe/TicTacToeWeb.java
@@ -8,36 +8,27 @@ import java.net.ResponseCache;
 
 public class TicTacToeWeb implements SparkApplication {
 
-	public static void main(String[] args){
-		staticFileLocation("/public");
-		SparkApplication TicTacToeWeb = new TicTacToeWeb();
-		String port = System.getenv("PORT");
-		if(port != null){
-			setPort(Integer.valueOf(port));
-		}
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        SparkApplication TicTacToeWeb = new TicTacToeWeb();
+        String port = System.getenv("PORT");
+        if (port != null) {
+            setPort(Integer.valueOf(port));
+        }
+        TicTacToeWeb.init();
+    }
+
+    public void init() {
+
         final TicTacToe game = new TicTacToe();
 
-        get(new Route("/"){
+        post(new Route("/reset") {
             @Override
-            public Object handle(Request request, Response responce){
-                String title = "TicTacToe";
-                String reset_game = "<a href='/index/reset'>Reset game</a>";
-                StringBuilder html = new StringBuilder();
-
-                html.append("<h1>").append(title).append("</h1>").append(reset_game);
-                html.append("<hr>");
-
-                game.resetBoard();
-                return html.toString();
-            }
-        });
-        post(new Route("/reset"){
-            @Override
-            public Object handle(Request request, Response response){
+            public Object handle(Request request, Response response) {
                 game.resetBoard();
                 response.status(200);
                 return response;
             }
         });
-	}
+    }
 }
