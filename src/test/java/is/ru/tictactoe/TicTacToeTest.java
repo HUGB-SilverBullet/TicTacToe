750baa266a8ce33becc16ff678fea6a23f837c64
Tryygvi Þór Tryggvason <tryggvi13@i-a4933858.greenqloud.internal>
2014-11-01 17:02:11 +0000

    fixed tests concerning playermove change

--- src/test/java/is/ru/tictactoe/TicTacToeTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeTest.java
@@ -29,35 +29,35 @@ public class TicTacToeTest{
 	{
 		TicTacToe test = new TicTacToe();
 		test.playerMove(1,1);
-		assertEquals('x',test.board[1][1]);
+		assertEquals('x',test.board[1 - 1][1 - 1]);
 		assertEquals(false, test.playerMove(1,1));
-		assertEquals(false, test.playerMove(3,3));
+		assertEquals(false, test.playerMove(4,4));
 	}
 	@Test
 	public void testCheckDiagonal()
 	{
 		TicTacToe test = new TicTacToe();
-		test.playerMove(0,0);
 		test.playerMove(1,1);
 		test.playerMove(2,2);
+		test.playerMove(3,3);
 		assertEquals(true, test.checkDiagonal());
 	}
 	@Test
 	public void testCheckRows()
 	{
 		TicTacToe test = new TicTacToe();
-		test.playerMove(0,0);
-                test.playerMove(0,1);
-                test.playerMove(0,2);
+		test.playerMove(1,1);
+                test.playerMove(1,2);
+                test.playerMove(1,3);
 		assertEquals(true, test.checkRows());
 	}
 	@Test
 	public void testCheckColumns()
 	{
 		TicTacToe test = new TicTacToe();
-		test.playerMove(0,0);
-                test.playerMove(1,0);
-                test.playerMove(2,0);
+		test.playerMove(1,1);
+                test.playerMove(2,1);
+                test.playerMove(3,1);
 		assertEquals(true, test.checkColumns());
 	}
 	@Test
